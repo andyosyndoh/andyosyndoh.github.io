bd1378cef7427cc7d1a44e8a66de4515598f499e
aosindo <andyovvo8@gmail.com>
2024-09-18 13:18:31 +0300

    chore: added test files

---
+++ utils/utils_test.go
@@ -0,0 +1,110 @@
+package utils
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestGetsingleartist(t *testing.T) {
+	type args struct {
+		ID int
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    Artists
+		wantErr bool
+	}{
+		{name: "Queen", args: args{ID: 1}, want: Artists{ID: 1, ImageURL: "https://groupietrackers.herokuapp.com/api/images/queen.jpeg", Name: "Queen", Members: []string{"Freddie Mercury", "Brian May", "John Daecon", "Roger Meddows-Taylor", "Mike Grose", "Barry Mitchell", "Doug Fogie"}, CreationDate: 1970, FirstAlbum: "14-12-1973", LocationsURL: "https://groupietrackers.herokuapp.com/api/locations/1", ConcertDatesURL: "https://groupietrackers.herokuapp.com/api/dates/1", RelationsURL: "https://groupietrackers.herokuapp.com/api/relation/1"}, wantErr: false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := Getsingleartist(tt.args.ID)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Getsingleartist() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Getsingleartist() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestGetLocations(t *testing.T) {
+	type args struct {
+		ID int
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    Location
+		wantErr bool
+	}{
+		{name: "Queen", args: args{ID: 1}, want: Location{ID: 1, Locations: []string{"north_carolina-usa", "georgia-usa", "los_angeles-usa", "saitama-japan", "osaka-japan", "nagoya-japan", "penrose-new_zealand", "dunedin-new_zealand"}, Dates: "https://groupietrackers.herokuapp.com/api/dates/1"}, wantErr: false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := GetLocations(tt.args.ID)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GetLocations() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("GetLocations() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestGetDates(t *testing.T) {
+	type args struct {
+		ID int
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    Date
+		wantErr bool
+	}{
+		{name: "Queen", args: args{ID: 1}, want: Date{ID: 1, Dates: []string{"*23-08-2019", "*22-08-2019", "*20-08-2019", "*26-01-2020", "*28-01-2020", "*30-01-2019", "*07-02-2020", "*10-02-2020"}}, wantErr: false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := GetDates(tt.args.ID)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GetDates() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("GetDates() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestGetRelation(t *testing.T) {
+	type args struct {
+		ID int
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    ArtistDetails
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := GetRelation(tt.args.ID)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GetRelation() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("GetRelation() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
