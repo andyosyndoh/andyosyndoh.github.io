7516afac620a65cfdab27dd932979788e3376e83
aosindo <andyovvo8@gmail.com>
2024-09-18 11:47:12 +0300

    chore: added routes checkers

---
+++ internals/routes/routes.go
@@ -0,0 +1,46 @@
+package routes
+
+import (
+	"net/http"
+	"strings"
+
+	"groupie/internals/handlers"
+	"groupie/internals/renders"
+)
+
+// Allowed routes
+var allowedRoutes = map[string]bool{
+	"/":        true,
+	"/details": true,
+}
+
+// RouteChecker is a middleware that checkes allowed routes
+func RouteChecker(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if strings.HasPrefix(r.URL.Path, "/static/") {
+			next.ServeHTTP(w, r)
+			return
+		}
+
+		if _, ok := allowedRoutes[r.URL.Path]; !ok {
+			handlers.NotFoundHandler(w, r)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
+// RegisterRoutes manages the routes
+func RegisterRoutes(mux *http.ServeMux) {
+	staticDir := renders.GetProjectRoot("views", "static")
+	fs := http.FileServer(http.Dir(staticDir))
+	mux.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		handlers.HomeHandler(w, r)
+	})
+
+	mux.HandleFunc("/details", func(w http.ResponseWriter, r *http.Request) {
+		handlers.Location(w, r)
+	})
+}
